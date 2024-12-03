3c267200c45f824961bb3e6bf82caec13792ae06
Timothy Jacobson <tim@jaconet.uk>
2024-12-03 22:07:06 +0000

    add getPuzzleInputWithoutSplit util function

--- src/main/kotlin/utils/PuzzleUtils.kt
+++ src/main/kotlin/utils/PuzzleUtils.kt
@@ -1,3 +1,4 @@
 package utils
 
 fun Any.getPuzzleInput() = this::class.java.getResource("input").readText().split("\n")
+fun Any.getPuzzleInputWithoutSplit() = this::class.java.getResource("input").readText()
