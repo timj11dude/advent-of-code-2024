2c0cb287254830e7150b70ed0001e1a062ae8fae
Timothy Jacobson <tim@jaconet.uk>
2024-12-05 09:52:32 +0000

    Add middle util function

--- src/main/kotlin/utils/PuzzleUtils.kt
+++ src/main/kotlin/utils/PuzzleUtils.kt
@@ -2,3 +2,12 @@ package utils
 
 fun Any.getPuzzleInput() = this::class.java.getResource("input").readText().split("\n")
 fun Any.getPuzzleInputWithoutSplit() = this::class.java.getResource("input").readText()
+
+/**
+ * Get the middle entry of a list
+ * @throws IllegalArgumentException if list is even sized
+ */
+fun <T> List<T>.middle(): T {
+    require(this.size % 2 == 1) { "need an odd sized list to find middle: [$this]" }
+    return this[this.size / 2]
+}
