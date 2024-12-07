3b0281e9b6f0323fe2e0f372a8b152faaa60a65e
Timothy Jacobson <tim@jaconet.uk>
2024-12-07 07:35:31 +0000

    wip: day 6 part 2

--- src/main/kotlin/day06/solution.kt
+++ src/main/kotlin/day06/solution.kt
@@ -1,6 +1,11 @@
 package day06
 
 import utils.*
+import java.lang.IllegalArgumentException
+
+private typealias Coords = Pair<Int,Int>
+private val Coords.x get() = first
+private val Coords.y get() = second
 
 object solution {
     private val example1 = """
@@ -16,41 +21,54 @@ object solution {
         ......#...
     """.trimIndent()
 
-    private val vectorSequence = sequence {
-        val list = listOf(
-            Vector2D.Companion.cardinal.BT,
-            Vector2D.Companion.cardinal.LR,
-            Vector2D.Companion.cardinal.TB,
-            Vector2D.Companion.cardinal.RL
-        )
-        while (true) {
-                yieldAll(list)
-            }
-        }
+    private val startVector = Vector2D.Companion.cardinal.BT
+    private val vectorSequence = generateSequence(startVector, ::getNextVector)
+    private fun getNextVector(current: Vector2D) = when(current) {
+        Vector2D.Companion.cardinal.BT -> Vector2D.Companion.cardinal.LR
+        Vector2D.Companion.cardinal.LR -> Vector2D.Companion.cardinal.TB
+        Vector2D.Companion.cardinal.TB -> Vector2D.Companion.cardinal.RL
+        Vector2D.Companion.cardinal.RL -> Vector2D.Companion.cardinal.BT
+        else -> throw IllegalArgumentException("not valid vector")
+    }
 
     private const val OBSTACLE = '#'
 
     private fun findStartPosition(matrix: Matrix<Char>) = matrix.indices.find { (x, y) -> matrix[x,y] == '^' }!!
 
+    private fun Matrix<Char>.isInLoop(visitedCoords: List<Coords>, startVector: Vector2D, newObstacle: Coords) = (vectorSequence.dropWhile { it != startVector }).runningFold(visitedCoords.last()) { (x, y), vector ->
+        getIteratorFromVectorWithIndex(x,y, vector).asSequence()
+            .takeWhile { (coords, v) -> v != OBSTACLE  && coords != newObstacle }
+            .last().first
+    }.let { seq ->
+        seq.runningFold(visitedCoords.dropLast(1).toSet()) { previous, current ->
+            previous + current
+        }
+            .zipWithNext()
+            .takeWhile { (a, b) -> a.size != b.size }
+            .last().let { (_, b) -> isNotOnEdge(b.last()) }
+    }
+
+    private fun <T> Matrix<T>.isNotOnEdge(coords: Coords) = coords.let { (x, y) -> !(x == 0 || y == 0 || x == maxX || y == maxY) }
+
     private fun solve(input: List<String>): Int {
         val grid = Matrix(input.map(String::toList))
         val start = findStartPosition(grid)
-        fun isNotOnEdge(coords: Pair<Int, Int>) = coords.let { (x, y) -> !(x == 0 || y == 0 || x == grid.maxX || y == grid.maxY) }
-        val distanceTraveled = vectorSequence.runningFold(start) { (x, y), vector ->
-            grid.getIteratorFromVectorWithIndex(x, y, vector)
+        val distanceTraveled = vectorSequence.drop(1).runningFold<Vector2D, Pair<Pair<Int, Int>, Vector2D>>(start to startVector) { (coords, _) , vector ->
+            val x = grid.getIteratorFromVectorWithIndex(coords.x, coords.y, vector)
                 .asSequence()
                 .takeWhile { (_, v) ->
                     v != OBSTACLE
-                }.last().first
+                }.last()
+            (x.first to getNextVector(vector))
         }
-            .zipWithNext()
-            .takeWhile { (a, _) -> isNotOnEdge(a) }
-            .flatMap { (a, b) -> (minOf(a.first,b.first)..maxOf(a.first,b.first)).flatMap { x ->
-                (minOf(a.second,b.second)..maxOf(a.second, b.second)).map { y ->
-                    x to y
-                }
-            } }
-            .distinct()
+            .let {seq ->
+                seq.runningFold(emptyList<Pair<Coords,Vector2D>>()) { previous, current -> previous + current }
+                    .filter { it.size >= 2 }
+                    .map { visitedCoords ->
+                        visitedCoords
+                    }
+
+            }
         return distanceTraveled.count()
     }
 
